7df1f18455f9b8ff97933f673db201ec807c90ea
Matt Chapman <eatsalldaylol@hotmail.com>
2021-10-15 23:21:15 -0400

    Theme button svg

--- assets/js/global_functions.js
+++ assets/js/global_functions.js
@@ -1,3 +1,13 @@
 const toggleThemeType = () => {
-    alert("Toggle");
+    let btn_theme_txt = document.getElementById("btn_theme_txt");
+    let btn_theme_img = document.getElementById("btn_theme_img");
+
+    if (btn_theme_txt.textContent === "Light Theme"){
+        btn_theme_txt.textContent = "Dark Theme";
+        btn_theme_img.src = "assets/images/icons/dark_mode_black_24dp.svg";
+    }else{
+        btn_theme_txt.textContent = "Light Theme";
+        btn_theme_img.src = "assets/images/icons/light_mode_black_24dp.svg";
+    }
+    
 }
