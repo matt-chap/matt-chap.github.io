b66c379573ef0b0d2932e6a9a8442c999703ae9b
Matt Chapman <eatsalldaylol@hotmail.com>
2021-10-17 22:45:19 -0400

    SVG attributes change

--- _site/assets/js/global_functions.js
+++ _site/assets/js/global_functions.js
@@ -1,12 +1,12 @@
 const toggleThemeType = () => {
     let btn_theme_txt = document.getElementById("btn_theme_txt");
     let btn_theme_svg_src = document.getElementById("btn_theme_svg_src"); //TODO: fix svg switch
-
+    
     if (btn_theme_txt.textContent === "Dark Theme: Off"){
         btn_theme_txt.textContent = "Dark Theme: On";
-        btn_theme_svg_src.href = "assets/images/icons/modes.svg#dark_mode";
+        btn_theme_svg_src.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#dark_mode');
     }else{
         btn_theme_txt.textContent = "Dark Theme: Off";
-        btn_theme_svg_src.href = "assets/images/icons/modes.svg#light_mode";
+        btn_theme_svg_src.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#light_mode');
     }
 }
