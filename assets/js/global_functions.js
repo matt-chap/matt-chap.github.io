a49a531585e3eb873d24675f06991bf8f7a4737d
Matt Chapman <eatsalldaylol@hotmail.com>
2021-10-18 22:05:01 -0400

    SVG change on click

--- assets/js/global_functions.js
+++ assets/js/global_functions.js
@@ -1,12 +1,13 @@
 const toggleThemeType = () => {
     let btn_theme_txt = document.getElementById("btn_theme_txt");
-    let btn_theme_svg_src = document.getElementById("btn_theme_svg_src"); //TODO: fix svg switch
-    
+    let btn_theme_svg_src = document.getElementById("btn_theme_svg_src");
+    let svg_href = btn_theme_svg_src.href.baseVal;
+
     if (btn_theme_txt.textContent === "Dark Theme: Off"){
         btn_theme_txt.textContent = "Dark Theme: On";
-        btn_theme_svg_src.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#dark_mode'); // TODO: does not change actual svg
-    }else{
+        btn_theme_svg_src.href.baseVal = svg_href.replace('light_mode','dark_mode');
+    } else {
         btn_theme_txt.textContent = "Dark Theme: Off";
-        btn_theme_svg_src.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#light_mode');
+        btn_theme_svg_src.href.baseVal = svg_href.replace('dark_mode','light_mode' );
     }
 }
