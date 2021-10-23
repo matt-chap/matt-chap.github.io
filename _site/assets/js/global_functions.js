1076e33fb4a86b53b91222f2ae82b097c3b141e5
Matt Chapman <eatsalldaylol@hotmail.com>
2021-10-22 23:07:01 -0400

    Theme button works

--- _site/assets/js/global_functions.js
+++ _site/assets/js/global_functions.js
@@ -6,8 +6,12 @@ const toggleThemeType = () => {
     if (btn_theme_txt.textContent === "Dark Theme: Off"){
         btn_theme_txt.textContent = "Dark Theme: On";
         btn_theme_svg_src.href.baseVal = svg_href.replace('light_mode','dark_mode');
+        document.body.classList.remove("theme--light");
+        document.body.classList.add("theme--dark");
     } else {
         btn_theme_txt.textContent = "Dark Theme: Off";
         btn_theme_svg_src.href.baseVal = svg_href.replace('dark_mode','light_mode' );
+        document.body.classList.remove("theme--dark");
+        document.body.classList.add("theme--light");
     }
 }
