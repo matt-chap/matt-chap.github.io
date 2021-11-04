708e447d87cab4d77a261a4e7e4af4502f2fa9b2
Matt Chapman <eatsalldaylol@hotmail.com>
2021-11-03 22:16:41 -0400

    Dark theme button changes

--- _site/assets/js/global_functions.js
+++ _site/assets/js/global_functions.js
@@ -1,7 +1,7 @@
 const storageName = "github_matt_theme";
 
 const toggleThemeType = () => {
-    setThemeContent(document.getElementById("btn_theme_txt").textContent === "Dark Theme: Off");
+    setThemeContent(document.getElementById('btn_theme_svg_src').href.baseVal.includes('light_mode'));
 }
 
 const loadThemeData = () => {
@@ -10,18 +10,15 @@ const loadThemeData = () => {
 }
 
 const setThemeContent = (isLightMode) => {
-    let btn_theme_txt = document.getElementById("btn_theme_txt");
     let btn_theme_svg_src = document.getElementById("btn_theme_svg_src");
     let svg_href = btn_theme_svg_src.href.baseVal;
 
     if (isLightMode){
-        btn_theme_txt.textContent = "Dark Theme: On";
         btn_theme_svg_src.href.baseVal = svg_href.replace('light_mode','dark_mode');
         document.body.classList.remove("theme--light");
         document.body.classList.add("theme--dark");
         localStorage.setItem(storageName, "Dark_Mode_On");
     } else {
-        btn_theme_txt.textContent = "Dark Theme: Off";
         btn_theme_svg_src.href.baseVal = svg_href.replace('dark_mode','light_mode' );
         document.body.classList.remove("theme--dark");
         document.body.classList.add("theme--light");
