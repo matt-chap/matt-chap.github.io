981fb08c9b2ed98769b2edd1d46c59b145c8a47c
Matt Chapman <eatsalldaylol@hotmail.com>
2021-11-04 20:09:31 -0400

    Theme button minimize

--- assets/js/global_functions.js
+++ assets/js/global_functions.js
@@ -1,27 +1,27 @@
 const storageName = "github_matt_theme";
 
 const toggleThemeType = () => {
-    setThemeContent(document.getElementById('btn_theme_svg_src').href.baseVal.includes('light_mode'));
+    setThemeContent(document.getElementById('btn_theme_svg_src').href.baseVal.includes('dark_mode'));
 }
 
 const loadThemeData = () => {
     let ls = localStorage.getItem(storageName);
-    setThemeContent(ls === "Dark_Mode_On");
+    setThemeContent(ls === "Dark_Mode_Off");
 }
 
-const setThemeContent = (isLightMode) => {
+const setThemeContent = (isDarkMode) => {
     let btn_theme_svg_src = document.getElementById("btn_theme_svg_src");
     let svg_href = btn_theme_svg_src.href.baseVal;
 
-    if (isLightMode){
-        btn_theme_svg_src.href.baseVal = svg_href.replace('light_mode','dark_mode');
-        document.body.classList.remove("theme--light");
-        document.body.classList.add("theme--dark");
-        localStorage.setItem(storageName, "Dark_Mode_On");
-    } else {
+    if (isDarkMode){
         btn_theme_svg_src.href.baseVal = svg_href.replace('dark_mode','light_mode' );
         document.body.classList.remove("theme--dark");
         document.body.classList.add("theme--light");
         localStorage.setItem(storageName, "Dark_Mode_Off");
+    } else {
+        btn_theme_svg_src.href.baseVal = svg_href.replace('light_mode','dark_mode');
+        document.body.classList.remove("theme--light");
+        document.body.classList.add("theme--dark");
+        localStorage.setItem(storageName, "Dark_Mode_On");
     }
 }
